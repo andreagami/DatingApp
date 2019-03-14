7312311873db54bb413edb158d4ad7ce30461ac6
Andre Felipe dos Santos <andrefelipe.ecp@gmail.com>
2019-03-14 16:51:19 +0000

    Add login/registration page with authentication

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,7 +19,9 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
+
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -28,6 +30,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
